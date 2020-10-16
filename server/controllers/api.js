c5106442a3675e4d049d0a1b086b09b7a26e9b6a
KyungMin Park <kyung-min.park1@students.mq.edu>
2020-10-17 00:11:19 +1100

    Complete registration & registration rest tests

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -13,6 +13,41 @@ const getUser = (id) => {
     return data.users.filter(u => u.id === id)[0]
 }
 
+apiRouter.post('/api/users', (req, res) => {
+    
+    const body = req.body
+
+    if (!body.id || !body.password || !body.firstname || !body.lastname || !body.email) {
+        return res.status(401).json({error: 'Please complete the form.'})
+    }
+
+    const idCheck = data.users.find(user => user.id === body.id)
+    const emailCheck = data.users.find(user => user.email === body.email)
+
+    if (idCheck) {
+        return res.status(401).json({error: 'Id is already exist'})
+    }
+
+    if (emailCheck) {
+        return res.status(401).json({error: 'email is already exist'})
+    }
+
+    bcrypt.hash(body.password, 10)
+    .then(result => {
+        
+        const newUser = {
+            id: body.id,
+            password: result,
+            firstname: body.firstname,
+            lastname: body.lastname,
+            email: body.email
+        }
+    
+        data.users.push(newUser)
+        return res.json('Complete the registration! Please log in to start shopping.')
+    })
+})
+
 apiRouter.post('/api/login', async (req, res) => {
     const {id, password} = req.body
 
