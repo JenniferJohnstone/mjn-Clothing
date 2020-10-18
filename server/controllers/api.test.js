63c3dda73818ce08c2b96d7a4bceb77ef118da81
KyungMin Park <kyung-min.park1@students.mq.edu>
2020-10-18 16:03:59 +1100

    backend test error (not working)

---
+++ server/controllers/api.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment node
+ */
+
+const supertest = require('supertest')
+const fs = require('fs')
+const app = require('./api.js')
+
+const api = supertest(app)
+
+const sampleData = (fileName) => {
+    const rawData = fs.readFileSync(fileName)
+    const data = JSON.parse(rawData)
+}
+
+describe('api', () => {
+
+    beforeEach(async () => {
+        sampleData('server/sample.json')
+    })
+
+    test('login works with correct username/password', async () => {
+
+        const data = {
+            id: 'Bobalooba',
+            password: 'bob'
+        }
+
+        await api.post('/api/login')
+                 .send(data)
+                 .expect(200)
+    })
+})
