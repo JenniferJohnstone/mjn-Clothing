f52b5adc593901364ce6e96d5de72940492f24fd
KyungMin Park <kyung-min.park1@students.mq.edu>
2020-10-18 15:44:09 +1100

    remove mongodb config codes

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    mongodbMemoryServerOptions: {
-        binary: {
-            version: '4.0.3',
-            skipMD5: true
-        },
-        autoStart: false,
-        instance: {}
-    }
+    
 };
